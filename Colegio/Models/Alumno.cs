f9db9492ee8145050a9a241b9df3d1840c625e53
Miguel Pelaez <mpelaez@personalsoft.com.co>
2017-10-31 11:25:44 -0500

    Herencia de alumno y persona.

---
+++ Colegio/Models/Alumno.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colegio.Models
+{
+    public class Alumno : Persona
+    {
+        public string Grupo { get; set; }
+    }
+}
--- Colegio/Program.cs
+++ Colegio/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Colegio.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,23 @@ namespace Colegio
         private static void Main(string[] args)
         {
             Console.WriteLine("Gestión del Colegio");
+
+            Alumno alumno = new Alumno
+            {
+                Id = 1,
+                Nombre = "Miguel",
+                Apellido = "Pelaez",
+                Grupo = "11C"
+            };
+
+            Persona persona = new Persona()
+            {
+                Id = 1,
+                Nombre = "Carlos",
+                Apellido = "Sanjuan"
+            };
+
+            Console.WriteLine($"El alumno es: {alumno.NombreCompleto}");
 
             Console.ReadLine();
         }
