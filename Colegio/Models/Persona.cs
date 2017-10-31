63dd6b58e81cc07c23ccb97abce5045e159d02fd
Miguel Pelaez <mpelaez@personalsoft.com.co>
2017-10-31 11:16:16 -0500

    Encapsulamiento

---
+++ Colegio/Models/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colegio.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public short Edad { get; set; }
+        public string Telefono { get; set; }
+
+        private string _segundoApellido;
+
+        public string SegundoApellido
+        {
+            get { return _segundoApellido; }
+            set { _segundoApellido = value; }
+        }
+
+        public string NombreCompleto
+        {
+            get
+            {
+                //return Nombre + " " + Apellido;
+                //return string.Format("{0} {1}", Nombre, Apellido);
+                return $"{Nombre} {Apellido}";
+            }
+        }
+    }
+}
