f4070ad650162af6b0002f62bce9218616f3a7e8
Miguel Pelaez <mpelaez@personalsoft.com.co>
2017-10-31 11:01:47 -0500

    Crear Proyecto consola y configuración del main.

---
+++ Colegio/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colegio
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Gestión del Colegio");
+
+            Console.ReadLine();
+        }
+    }
+}
